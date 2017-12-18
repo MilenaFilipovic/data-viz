be165c98e8bef385a0a5a0bb3485f8947fda8408
Lia <liabifano@gmail.com>
2017-12-18 15:43:27 +0100

    fix issue of dots

--- app/metrics-helpers.js
+++ app/metrics-helpers.js
@@ -22,15 +22,8 @@ function drawMetrics(languageStats, allStats, statistic) {
         .filter(x => x['statistic'] == statistic & x['metric'] == 'mean')
         .map(x => [x['cohort'], x['value']]);
 
-    let yMax = filteredStats.concat(geralMean).map(x => x[1])
-        .reduce(function (a, b) {
-            return Math.max(a,b) + 10
-        });
-
-    let yMin = math.max(0, filteredStats.concat(geralMean).map(x => x[1])
-        .reduce(function (a, b) {
-            return Math.min(a, b) - 10
-        }));
+    let yMax = math.max(filteredStats.concat(geralMean).map(x => x[1])) + 20,
+        yMin = math.max(0, math.min(filteredStats.concat(geralMean).map(x => x[1])) - 20);
 
     let svg = d3.select("#side-menu")
         .append("svg:svg")
@@ -95,7 +88,15 @@ function drawMetrics(languageStats, allStats, statistic) {
         .attr("stroke-width", 1.2)
         .attr("d", line);
 
-    var focus = g.append("g")
+    g.append("text")
+        .attr("x", (widthMetrics / 2))
+        .attr("y", 0 - (marginMetrics.top / 2))
+        .attr("text-anchor", "middle")
+        .style("font-size", "14px")
+        .style("font-weight", "bold")
+        .text(metricName);
+
+    let focus = g.append("g")
         .attr("class", "focus")
         .style("display", "none");
 
@@ -110,13 +111,15 @@ function drawMetrics(languageStats, allStats, statistic) {
         .attr("x2", widthMetrics);
 
     focus.append("circle")
-        .attr("r", 7.5);
+        .attr("r", 3);
 
     focus.append("text")
         .attr("x", 15)
-        .attr("dy", ".31em");
+        .attr("dy", "-.45em")
+        .attr("font-size", "12px")
+        .attr('font-weight', 'bold');
 
-    let bisectDate = d3.bisector(function(d) { return d.year; }).left;
+    let bisectDate = d3.bisector(function(d) { return d[0]; }).left;
 
     svg.append("rect")
         .attr("transform", "translate(" + marginMetrics.left + "," + marginMetrics.top + ")")
@@ -134,18 +137,10 @@ function drawMetrics(languageStats, allStats, statistic) {
             d1 = filteredStats[i],
             d = x0 - d0.year > d1.year - x0 ? d1 : d0;
         focus.attr("transform", "translate(" + x(d[0]) + "," + y(d[1]) + ")");
-        focus.select("text").text(function() { return d[1]; });
+        focus.select("text").text(function() { return math.round(d[1], 1); });
         focus.select(".x-hover-line").attr("y2", heightMetrics - y(d[1]));
         focus.select(".y-hover-line").attr("x2", widthMetrics + widthMetrics);
     }
-
-    g.append("text")
-        .attr("x", (widthMetrics / 2))
-        .attr("y", 0 - (marginMetrics.top / 2))
-        .attr("text-anchor", "middle")
-        .style("font-size", "14px")
-        .style("font-weight", "bold")
-        .text(metricName);
 }
 
 
@@ -161,15 +156,8 @@ function drawGeralMetrics(geralStatistcs, statistic, language) {
         .filter(x => x['language'] == 'all')
         .map(x => [x['cohort'], x[statistic]]);
 
-    let yMax = filteredStats.concat(geralMean).map(x => x[1])
-        .reduce(function (a, b) {
-            return Math.max(a,b) + 10
-        });
-
-    let yMin = filteredStats.concat(geralMean).map(x => x[1])
-        .reduce(function (a, b) {
-            return Math.min(a, b) - 10
-        });
+    let yMax = math.max(filteredStats.concat(geralMean).map(x => x[1])) + 20,
+        yMin = math.max(0, math.min(filteredStats.concat(geralMean).map(x => x[1])) - 20);
 
     let svg = d3.select("#side-menu")
         .append("svg:svg")
@@ -234,10 +222,6 @@ function drawGeralMetrics(geralStatistcs, statistic, language) {
         .attr("stroke-width", 1.2)
         .attr("d", line);
 
-    var focus = g.append("g")
-        .attr("class", "focus")
-        .style("display", "none");
-
     g.append("text")
         .attr("x", (widthMetrics / 2))
         .attr("y", 0 - (marginMetrics.top / 2))
@@ -245,6 +229,53 @@ function drawGeralMetrics(geralStatistcs, statistic, language) {
         .style("font-size", "14px")
         .style("font-weight", "bold")
         .text(metricName);
+
+    let focus = g.append("g")
+        .attr("class", "focus")
+        .style("display", "none");
+
+    focus.append("line")
+        .attr("class", "x-hover-line hover-line")
+        .attr("y1", 0)
+        .attr("y2", heightMetrics);
+
+    focus.append("line")
+        .attr("class", "y-hover-line hover-line")
+        .attr("x1", widthMetrics)
+        .attr("x2", widthMetrics);
+
+    focus.append("circle")
+        .attr("r", 3);
+
+    focus.append("text")
+        .attr("x", 15)
+        .attr("dy", "-.45em")
+        .attr("font-size", "12px")
+        .attr('font-weight', 'bold');
+
+    let bisectDate = d3.bisector(function(d) { return d[0]; }).left;
+
+    svg.append("rect")
+        .attr("transform", "translate(" + marginMetrics.left + "," + marginMetrics.top + ")")
+        .attr("class", "overlay")
+        .attr("width", widthMetrics)
+        .attr("height", heightMetrics)
+        .on("mouseover", function() { focus.style("display", null); })
+        .on("mouseout", function() { focus.style("display", "none"); })
+        .on("mousemove", mousemove);
+
+    function mousemove() {
+        var x0 = x.invert(d3.mouse(this)[0]),
+            i = bisectDate(filteredStats, x0, 1),
+            d0 = filteredStats[i - 1],
+            d1 = filteredStats[i],
+            d = x0 - d0.year > d1.year - x0 ? d1 : d0;
+        focus.attr("transform", "translate(" + x(d[0]) + "," + y(d[1]) + ")");
+        focus.select("text").text(function() { return math.round(d[1], 1); });
+        focus.select(".x-hover-line").attr("y2", heightMetrics - y(d[1]));
+        focus.select(".y-hover-line").attr("x2", widthMetrics + widthMetrics);
+    }
 }
+
 
 
